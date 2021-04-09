bf091c39f594969a1c161ddcf90bb037b7251888
donysRafa <denys.nobre@outlook.com>
2021-04-09 12:03:37 -0300

    Avanço nas interfaces relativas ao funcionario
    Progresso na interface de cadastro e na classe correspondente para verificar se fora colocado ou não as informações certas nas text field, e também cadastra-lo no banco de dados.

--- src/br/ufrn/imd/controle/TelaCadastroFuncionarioController.java
+++ src/br/ufrn/imd/controle/TelaCadastroFuncionarioController.java
@@ -1,20 +1,63 @@
 package br.ufrn.imd.controle;
 
+import br.ufrn.imd.dao.Banco;
+import br.ufrn.imd.modelo.Funcionario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class TelaCadastroFuncionarioController {
 	
 	private Stage cadastroStage;
 	
+	private boolean novoFuncionarioCadastrado = false;
+	
 	@FXML
     private MenuItem menuItemFecharJanela;
 
 	@FXML
     private Button botaoCadastrarFuncionario;
+	
+	@FXML
+    private Button botaoEntrarMenuFuncionario;
+	
+	@FXML
+	private TextField textFieldNomeDoFuncionario;
+
+    @FXML
+    private TextField textFieldFuncaoDoFuncionario;
+
+    @FXML
+    private TextField textFieldResultadoCadastro;
+    
+    @FXML
+    void cadastrarFuncionario(ActionEvent event) {
+    	String input = textFieldNomeDoFuncionario.getText();
+    	String input2 = textFieldFuncaoDoFuncionario.getText();
+    	
+    	if(!input.equals("") && !input2.equals("")) {
+			Banco banco = Banco.getInstance();
+			
+			int id = banco.getBancoFuncionarios().getContadorParaId();
+			
+			Funcionario employer = new Funcionario();
+			
+			employer.setId(id);
+			
+			employer.setNome(input);
+			
+			banco.getBancoFuncionarios().cadastrarFuncionario(employer);
+			
+			textFieldResultadoCadastro.setText("Funcionario Nº: " + id + " cadastrado com sucesso!");
+			
+			novoFuncionarioCadastrado = true;
+    	}else {
+    		textFieldResultadoCadastro.setText("Por favor preencha seu nome e/ou função!");
+    	}
+    }
 
     @FXML
     void abrirTelaMenuFuncionario(ActionEvent event) {
